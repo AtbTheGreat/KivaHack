1adb35bce3b3d79fa9729e042605c3f6d17f712d
Arthur Harper <harper10@purdue.edu>
2015-03-20 09:18:38 -0400

    Trying to get Collection to work

--- meteor/kivahack/kivahack.js
+++ meteor/kivahack/kivahack.js
@@ -6,56 +6,60 @@ if (Meteor.isClient) {
   var search_url = "";
   var options = {};
   var query_params = {};
+
   Template.body.helpers({
     results: function () {
       return Results.find({});
-    }})
-    Template.hello.events({
-      "submit .lenders": function (event) {
-        // This function is called when the new task form is submitted
-        lender = event.target.text.value;
-        console.log(event.target.text.value);
-        console.log("Lenders")
-        // Clear form
-        event.target.text.value = "";
-      },
-      "submit .searchname": function (event) {
-        Session.set("stringSearched", event.target.q.value);
-        search_url = 'http://api.kivaws.org/v1/lenders/search.json';
-        query_params = {
-          q: Session.get("stringSearched"),
-          sort_by: "newest",
-          ids_only: "false"
-        };
-        options = {
-          data: query_params,
-          type: "GET",
-          dataType: 'json'
-        }
-
-        Results.remove({})
-        request = jQuery.ajax(search_url, options).done(showResponse);
-        return false;
+    }
+  })
+  Template.hello.events({
+    "input .searchname": function (event) {
+      console.log(event.target.value)
+      Session.set("stringSearched", event.target.value);
+      search_url = 'http://api.kivaws.org/v1/lenders/search.json';
+      query_params = {
+        q: Session.get("stringSearched"),
+        sort_by: "newest",
+        ids_only: "false"
+      };
+      options = {
+        data: query_params,
+        type: "GET",
+        dataType: 'json'
       }
+
+      console.log("starting search call")
+      request = $.ajax(search_url, options)
+      request.done(showResponse);
+
+      console.log("finished both search call")
+      return false;
+    }
+  });
+
+  var page = '';
+  var loan_id = 'text';
+
+
+
+  function showResponse(response) {
+    RESPONSE = response;
+    var list = Results.find({})
+    console.log(list.Collection)
+    $.each (list.Collection, function(i, data) {
+      console.log(data)
+      Results.remove(this._id)
+    })
+
+    $.each(response.lenders, function(i, lenderData) {
+      Results.insert({lender_id: lenderData.lender_id, data: lenderData.name+", "+lenderData.whereabouts})
     });
 
-    var page = '';
-    var loan_id = 'text';
-
-
-
-    function showResponse(response) {
-      RESPONSE = response;
-
-      $.each(response.lenders, function(i, lenderData) {
-        Results.insert({lender_id: lenderData.lender_id, data: lenderData.name+", "+lenderData.whereabouts})
-      });
-
-    }
-
-  };
-
-
-  if (Meteor.isServer) {
-
   }
+
+};
+
+
+if (Meteor.isServer) {
+
+}
